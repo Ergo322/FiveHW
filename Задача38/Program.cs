cb777f070689d597a97c20346091ee63903617e4
Ergo322 <Shikmen2@mail.ru>
2022-09-07 23:39:36 +0300

    task38

---
+++ Задача38/Program.cs
@@ -0,0 +1,27 @@
+﻿void DifferenceMinMax()
+{
+    int[] array = new int[5];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100);
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.WriteLine($"{array[i]}");
+    }
+    int min = array[0];
+    int max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    int raz = max - min;
+
+    Console.Write($"Разность между макс и мин ровна: {raz}");
+}
+DifferenceMinMax();
