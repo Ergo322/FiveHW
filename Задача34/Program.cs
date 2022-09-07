201d48eabcec22ce9c4f0f55e7e82941f57ec211
Ergo322 <Shikmen2@mail.ru>
2022-09-07 23:39:06 +0300

    task34

---
+++ Задача34/Program.cs
@@ -0,0 +1,18 @@
+﻿void EvenNum()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.WriteLine($"{array[i]}");
+    }
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    }
+    Console.WriteLine($"чётных чисел {count}");
+}
+EvenNum();
